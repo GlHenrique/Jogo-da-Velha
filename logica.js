d5b6a154b85ac1f82af7dbbc34221011001bc3d4
GlHenrique <gleniohenrique-carvalho@hotmail.com>
2019-04-10 11:56:36 -0300

    Add files via upload

--- logica.js
+++ logica.js
@@ -1,4 +1,5 @@
 var first_play = 1;
+var fim = "";
 var vet = [
             ["","",""],
             ["","",""],
@@ -9,13 +10,14 @@ var vet1 = [
     ["1","2","3"],
     ["4","5","6"],
     ["7","8","9"]
-
 ];
-
 for(var i=0; i<9; i++){
     (document.getElementById(toString(i)).innerHTML=""); 
-}
+};
 function play(id){
+    if(fim=="x"||fim=="o"){
+        return null;
+    }
     const el = document.getElementById(id);
 if(el.innerHTML=="") 
     if(first_play % 2 == 0){
@@ -43,40 +45,56 @@ var imagem = "o";
       }
 console.log(vet);
     first_play ++;
+fim = vitoria();
+console.log(fim)
 }
 function vitoria (){
     for(var i=0; i<3; i++){
         if(vet[i][0]==vet[i][1]&& vet[i][0]==vet[i][2]){
-            if(vet[0][0]=="x"){
+            if(vet[i][0]=="x"){
+                console.log(1)
+                alert('O VENCEDOR FOI X!');
                 return 'x';
+        
             }
-            else if (vet[0][0]=="o"){
+            else if (vet[i][0]=="o"){
+                alert( 'O VENCEDOR FOI "O" (BOLINHA)!');
                 return 'o';
             }    
         }
         else if(vet[0][i]==vet[1][i]&& vet[0][i]==vet[2][i]){
-            if(vet[0][0]=="x"){
+            if(vet[0][i]=="x"){
+                alert( 'O VENCEDOR FOI X!');
                 return 'x';
             }
-            else if (vet[0][0]=="o"){
+            else if (vet[0][i]=="o"){
+                alert( 'O VENCEDOR FOI "O" (BOLINHA)!');
                 return 'o';
             }    
         }
     }
     if(vet[0][0]==vet[1][1] && vet[0][0]==vet[2][2]){
         if(vet[0][0]=='x'){
+            alert( 'O VENCEDOR FOI X!');
             return 'x';
         }
         else if (vet[0][0]=="o"){
+            alert( 'O VENCEDOR FOI "O" (BOLINHA)!');
             return 'o';
         }    
     }
     if(vet[0][2]==vet[1][1] && vet[0][2]==vet[2][0]){
         if(vet[0][2]=='x'){
-            return 'x'
+            alert( 'O VENCEDOR FOI X!');
+            return 'x';
         }
         else if (vet[0][2]){
-            return 'o'
+            alert( 'O VENCEDOR FOI "O" (BOLINHA)!');
+            return 'o';
         }
     }
 }
+function newgame (){
+    window.location.reload()
+}
+
